0bd5a134ae0b75154720bd89445b3f13808cea84
Sadaru-Hansaka <sandaruhansaka0828@gmail.com>
2024-06-14 07:04:54 +0530

    Learned about javascript nested for loops and used css styles in side a for loop

--- JS/For-loops/script.js
+++ JS/For-loops/script.js
@@ -100,3 +100,22 @@ for (const singleObject in nestedObjects){
     UL4.append(itemlist2);
 }
 loop4.append(UL4);
+
+
+
+
+// nested for loops
+for (var year = 2023; year<2025;year++){
+    console.log(year);
+    for(var month=1;month<13;month++){
+        console.log("--------------------",month);
+    }
+}
+
+// add css to the for loop
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < 7; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
