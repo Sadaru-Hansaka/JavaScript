13c8bad30576f78fba19a7e962a3517ecac41537
Sadaru-Hansaka <sandaruhansaka0828@gmail.com>
2024-06-16 06:24:51 +0530

    got knowledge about Js data types

---
+++ JS/TypeOf/script.js
@@ -0,0 +1,29 @@
+
+var test1 = typeof("Name");
+console.log(test1);
+
+var test2 = typeof(10);
+console.log(test2);
+
+var test3 = typeof(3.45);
+console.log(test3);
+
+var test4 = typeof(true);
+console.log(test4);
+
+var test5 = typeof(false);
+console.log(test5);
+
+var test6 = typeof(12<50);
+console.log(test6)
+
+var test7 = typeof([1,2,3,4,5]);
+console.log(test7);
+
+var test8 = typeof({FirstName : "sadaru"});
+console.log(test8);
+
+var test9 = typeof(function abs(){
+    console.log("hello");
+})
+console.log(test9)
