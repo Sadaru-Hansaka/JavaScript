bee9fc0f86e6d9a42f1a616662b0d0b9cef3937a
Sadaru-Hansaka <sandaruhansaka0828@gmail.com>
2024-06-18 05:29:25 +0530

    Practiced some short codes and checked how they work

---
+++ JS/practice/script.js
@@ -0,0 +1,56 @@
+
+//  var sqrt = (function(x) {
+//   console.log(x*x);
+//  })(my_number)
+
+//  my_number = 5;
+
+//  const myArray = [1, 2, 3, 4]
+// myArray.forEach( (item, index) => {
+//     myArray[index] = ++item;
+// });
+
+// console.log(myArray)
+
+// const myFunction = (data) => {
+//     return;
+//     let newData = data + 1;
+//     return newData;
+//  }
+ 
+//  console.log(myFunction(5));
+
+// function myfunc(x,y) {
+//     return(x+y);
+//    }
+//    console.log(myfunc(2,myfunc(5,-2)));
+
+// try{
+//     console.log("Hello");
+// }catch(err){
+//     console.log("bye");
+// }
+
+// var x;
+
+// if(x === null) {
+//   console.log("null");
+// } else if(x === undefined) {
+//   console.log("undefined");
+// } else {
+//   console.log("ok");
+// }
+
+// var x = null;
+// console.log(x);
+
+
+// throw new Error();
+// console.log("Hello");
+
+try {
+    throw new Error();
+    console.log('Hello');
+} catch(err) {
+    console.log('Goodbye');
+}
