2d9c15245c60e8e5c5060d66a9c7c0a2f855f406
Sadaru-Hansaka <sandaruhansaka0828@gmail.com>
2024-06-21 05:37:19 +0530

    Learned about three built in methods related to objects

---
+++ OOP/Built_in_methods/script.js
@@ -0,0 +1,37 @@
+var clothingItem = {
+    price: 50,
+    color: 'beige',
+    material: 'cotton',
+    season: 'autumn'
+}
+
+for( const key of Object.keys(clothingItem) ) {
+    console.log(key, ":", clothingItem[key])
+}
+
+// ------------------------------
+
+console.log(" ");
+console.log("Acces dynamically either one of those proprties");
+
+function testBracketsDynamicAccess() {
+    var dynamicKey;
+    if(Math.random() > 0.5) {
+      dynamicKey = "speed";
+     }else{
+       dynamicKey = "color";
+     }
+  
+      var drone = {
+        speed: 15,
+        color: "orange"
+      }
+  
+      console.log(drone[dynamicKey]);
+  }
+  testBracketsDynamicAccess();
+
+//   -----------------------------
+// object.keys();
+// object.entries();
+// object.values();
