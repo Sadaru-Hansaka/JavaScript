7d12e7424b0d1ec836756643f28ecc28efe13d0d
Sadaru-Hansaka <sandaruhansaka0828@gmail.com>
2024-06-24 06:20:29 +0530

    Learned about rest operators

--- Rest Operator/script.js
+++ Rest Operator/script.js
@@ -1,5 +1,5 @@
 const top7 = ["Sadaru", "Ronaldo","Nethra","Kodagoda","Gamage","Hafiz","Hansaka"];
-
+console.log("Array destructuring")
 // rest operator
 const [] = top7;
 const [first, second, third, ...rest] = top7;
@@ -7,12 +7,21 @@ const [first, second, third, ...rest] = top7;
 console.log(top7);
 console.log(first);
 console.log(rest);
+console.log(" ")
 
 // use rest operator to functions
-
+console.log("Function Parameters")
 function sum(...numbers) {
     return numbers.reduce((total, num) => total + num, 0);
 }
 
 console.log(sum(1, 2, 3));
 console.log(sum(4, 5, 6, 7)); 
+console.log(" ")
+
+// object destructuring
+console.log("Object destructuring")
+const { a, b, ...rest1 } = { a: 1, b: 2, c: 3, d: 4 };
+console.log(a); // Output: 1
+console.log(b); // Output: 2
+console.log(rest1); // Output: { c: 3, d: 4 }
